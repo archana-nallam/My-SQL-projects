f5de2c671a50a096551eda07e0d9abd93926f8a5
Archana_Nallam <75180978+archana-nallam@users.noreply.github.com>
2022-10-07 20:27:41 -0700

    Add files via upload

---
+++ Capital_bike_share_data_2014_Q1/Queries/Window_functions/Row_number.sql
@@ -0,0 +1,11 @@
+ROW_NUMBER()
+
+ROW_NUMBER() does just what it sounds like—displays the number of a given row. It starts are 1 and numbers the rows according to the ORDER BY part of the window statement. ROW_NUMBER() does not require you to specify a variable within the parentheses:
+
+SELECT start_station,
+       start_date,
+       duration,
+       ROW_NUMBER() OVER (ORDER BY start_date desc)
+                    AS row_number
+ FROM 2014q1_capitalbikeshare
+ WHERE start_date < '2014-01-08'
