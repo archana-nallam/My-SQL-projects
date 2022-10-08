73e90ba4334fef59e03cea71cb5741cdf8450e83
Archana_Nallam <75180978+archana-nallam@users.noreply.github.com>
2022-10-07 23:05:49 -0700

    Update lead_lag.sql

--- Capital_bike_share_data_2014_Q1/Queries/Window_functions/lead_lag.sql
+++ Capital_bike_share_data_2014_Q1/Queries/Window_functions/lead_lag.sql
@@ -1,5 +1,7 @@
 LAG and LEAD
-It can often be useful to compare rows to preceding or following rows, especially if you've got the data in an order that makes sense. You can use LAG or LEAD to create columns that pull values from other rows—all you need to do is enter which column to pull from and how many rows away you'd like to do the pull. LAG pulls from previous rows and LEAD pulls from following rows:
+It can often be useful to compare rows to preceding or following rows, especially if you've got the data in an order that makes sense. 
+You can use LAG or LEAD to create columns that pull values from other rows—all you need to do is enter which column to pull from and 
+how many rows away you'd like to do the pull. LAG pulls from previous rows and LEAD pulls from following rows:
 
 
 SELECT start_station,
@@ -23,7 +25,9 @@ SELECT start_station,
  ORDER BY start_station, duration
 
 
-The first row of the difference column is null because there is no previous row from which to pull. Similarly, using LEAD will create nulls at the end of the dataset. If you'd like to make the results a bit cleaner, you can wrap it in an outer query to remove nulls:
+The first row of the difference column is null because there is no previous row from which to pull. Similarly, 
+using LEAD will create nulls at the end of the dataset. If you'd like to make the results a bit cleaner, 
+you can wrap it in an outer query to remove nulls:
 
 SELECT * from
 (SELECT start_station,
